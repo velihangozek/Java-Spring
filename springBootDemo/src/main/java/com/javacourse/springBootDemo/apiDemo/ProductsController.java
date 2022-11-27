32e4981df98adf051ddfc01aa40b468c5b64a0a2
Velihan Gözek <velihangozek@hotmail.com>
2022-11-28 01:55:27 +0300

    add springBootDemo project

---
+++ springBootDemo/src/main/java/com/javacourse/springBootDemo/apiDemo/ProductsController.java
@@ -0,0 +1,18 @@
+package com.javacourse.springBootDemo.apiDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductsController {
+	
+	@GetMapping("/")
+	public String get() {
+		return "Computer";
+	}
+	
+	@GetMapping("/products")
+	public String get2() {
+		return "Laptop";
+	}
+}
